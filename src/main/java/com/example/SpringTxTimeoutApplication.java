1db424066fd133b3dd4b32227668ceedf4cde2f5
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-04-28 09:09:24 +0900

    First commit

---
+++ src/main/java/com/example/SpringTxTimeoutApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTxTimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTxTimeoutApplication.class, args);
+	}
+}
